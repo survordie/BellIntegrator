0e09ad1b9298cb539a30b47c5b2ef96c0fea04a4
survordie <survordie@ya.ru>
2018-08-04 03:12:18 +0300

    country_id and doc_type_id were redirected

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,25 +5,10 @@ CREATE TABLE IF NOT EXISTS Country (
     code        VARCHAR(3) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS User (
-	id                  INTEGER  PRIMARY KEY AUTO_INCREMENT,
-	version    			INTEGER NOT NULL,
-	office_id           INTEGER NOT NULL,
-	doc_id              INTEGER NOT NULL,
-	first_name 			VARCHAR(50) NOT NULL,
-	second_name			VARCHAR(50),
-	middle_name			VARCHAR(50),
-	position			INTEGER NOT NULL,
-	phone				VARCHAR(20),
-	citizenship_name    VARCHAR(50),
-	citizenship_code	VARCHAR(50),
-	is_identified 		BOOLEAN
-);
-
 CREATE TABLE IF NOT EXISTS Organization (
 	id          INTEGER  PRIMARY KEY AUTO_INCREMENT,
 	version     INTEGER NOT NULL,
-	country_id  INTEGER NOT NULL,
+	country_id  VARCHAR(3) NOT NULL,
 	name		VARCHAR(50) NOT NULL,
 	full_name	VARCHAR(50) NOT NULL,
 	inn			VARCHAR(12) NOT NULL,
@@ -58,8 +43,23 @@ CREATE TABLE IF NOT EXISTS Doc (
     doc_date	DATE
 );
 
+CREATE TABLE IF NOT EXISTS User (
+	id                  INTEGER  PRIMARY KEY AUTO_INCREMENT,
+	version    			INTEGER NOT NULL,
+	office_id           INTEGER NOT NULL,
+	doc_id              INTEGER NOT NULL,
+	first_name 			VARCHAR(50) NOT NULL,
+	second_name			VARCHAR(50),
+	middle_name			VARCHAR(50),
+	position			INTEGER NOT NULL,
+	phone				VARCHAR(20),
+	citizenship_name    VARCHAR(50),
+	citizenship_code	VARCHAR(50),
+	is_identified 		BOOLEAN
+);
+
 CREATE INDEX IX_Organization_country_id ON Organization(country_id);
-ALTER TABLE Organization ADD FOREIGN KEY (country_id) REFERENCES Country(id);
+ALTER TABLE Organization ADD FOREIGN KEY (country_id) REFERENCES Country(code);
 
 
 CREATE INDEX IX_Office_organization_id ON Office(organization_id);
@@ -72,4 +72,4 @@ CREATE INDEX IX_User_doc_id ON User(doc_id);
 ALTER TABLE User ADD FOREIGN KEY (doc_id) REFERENCES Doc(id);
 
 CREATE INDEX IX_Doc_doc_type_id ON Doc(doc_type_id);
-ALTER TABLE Doc ADD FOREIGN KEY (doc_type_id) REFERENCES Doc_Type(id);
+ALTER TABLE Doc ADD FOREIGN KEY (doc_type_id) REFERENCES Doc_Type(code);
